80c1b3f1c090840a1e6ed218e605ee87bbca8620
yogaarmdhn <yogaarmdhn@gmail.com>
2020-12-26 23:06:50 +0700

    Commit Tugas Besar 2

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+            $('.navbar').addClass("bg-dark");
+        }else{
+            $('.navbar').removeClass("sticky");
+            $('.navbar').removeClass("bg-dark");
+        }
+    });
+});
+
+// typing animation script
+var typed = new Typed(".typing", {
+    strings: ["Student", "Photographer", "Videographer", "Web Develover"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
